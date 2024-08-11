d1cd9bbbcecf7c2fcdc1d62a29e3379a3e15790a
Vanshshah2325 <155459451+Vanshshah2325@users.noreply.github.com>
2024-08-11 11:23:25 +0530

    from my system
    TIC-TOC-TOE Game complete full project

---
+++ TicTacToe.js
@@ -0,0 +1,80 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset");
+let newgamebtn = document.querySelector("#new-btn");
+let msgcontainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turn0 = true;
+const winpatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+const resetgame = () => {
+    turn0 = true;
+    enableboxes();
+    msgcontainer.classList.add("hide");
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turn0) {
+            box.innerText = "O";
+            turn0 = false;
+        } else {
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+
+        checkwinner();
+    })
+});
+
+const disableboxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableboxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showwinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`; // Use backticks for template strings
+    msgcontainer.classList.remove("hide");
+    disableboxes();
+}
+
+const checkwinner = () => {
+    for (let pattern of winpatterns) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 != "" && pos2 != "" && pos3 != "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                showwinner(pos1);
+                return; // Stop checking if a winner is found
+            }
+        }
+    }
+    // Check for a draw
+    if ([...boxes].every(box => box.innerText !== "")) {
+        msg.innerText = "It's a Draw!";
+        msgcontainer.classList.remove("hide");
+    }
+};
+
+newgamebtn.addEventListener("click", resetgame); // Call `resetgame` on new game button click
+resetbtn.addEventListener("click", resetgame); // Call `resetgame` on reset button click
